7a16c4e17257e3aa42ec74f79e0a1efda68fe1f3
soonmae <tnsgh9603@naver.com>
2021-12-29 14:09:43 +0900

    21.12.29

---
+++ BOJ_CPP/23968.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    fastio;
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    int cnt = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - 1; ++j) {
+            if (v[j] > v[j + 1]) {
+                if (++cnt == k) {
+                    cout << v[j + 1] << " " << v[j];
+                    return 0;
+                }
+                swap(v[j], v[j + 1]);
+            }
+        }
+    }
+    cout << -1;
+    return 0;
+}
