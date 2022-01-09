26cdd404944eb72a5da78a8fccfbf67960d1c181
soonmae <tnsgh9603@naver.com>
2022-01-09 17:26:03 +0900

    22.01.09

---
+++ BOJ_CPP/24082.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    cout << (int) pow(n, 3);
+    return 0;
+}
