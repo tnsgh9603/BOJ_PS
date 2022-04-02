90506966dc3f6aad7d4f96f2793f114d57de8fbd
soonmae <tnsgh9603@naver.com>
2022-04-02 23:17:07 +0900

    22.04.02

---
+++ BOJ_CPP/24860.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    fastio;
+    long long a, b, c, d, e, f, g;
+    cin >> a >> b >> c >> d >> e >> f >> g;
+    cout << (a * b + c * d) * e * f * g;
+    return 0;
+}
